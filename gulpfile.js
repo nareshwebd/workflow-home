d40796f5bcee8966b02de0eaf2344dd5f054c0f5
webd <nareshweb@gmail.com>
2016-08-25 20:36:54 +0530

    home pc use

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
+	concats = require('gulp-concat');
+
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = ['components/scripts/*.js'];
+
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concats('script.js'))
+	.pipe(browserify())
+    .pipe(gulp.dest('build/developement/js'))
+});
